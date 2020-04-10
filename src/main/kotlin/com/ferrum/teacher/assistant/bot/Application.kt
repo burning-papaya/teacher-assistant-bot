30dda24d0237e2989275db192da0326a977b4248
temur.navoi <temur.navoi@yandex.com>
2020-04-10 11:40:50 +0500

    - set up of encrypted properties and logging

--- src/main/kotlin/com/ferrum/teacher/assistant/bot/Application.kt
+++ src/main/kotlin/com/ferrum/teacher/assistant/bot/Application.kt
@@ -5,11 +5,13 @@ Time: 21:57
  */
 package com.ferrum.teacher.assistant.bot
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.telegram.telegrambots.ApiContextInitializer
 
 @SpringBootApplication
+@EnableEncryptableProperties
 class Application
 fun main(args: Array<String>) {
     ApiContextInitializer.init()
--- src/main/kotlin/com/ferrum/teacher/assistant/bot/service/TeacherAssistantBot.kt
+++ src/main/kotlin/com/ferrum/teacher/assistant/bot/service/TeacherAssistantBot.kt
@@ -5,28 +5,35 @@ Time: 22:23
  */
 package com.ferrum.teacher.assistant.bot.service
 
-import org.apache.logging.log4j.LogManager
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import org.telegram.telegrambots.bots.TelegramLongPollingBot
 import org.telegram.telegrambots.meta.api.objects.Update
 
-
 @Component
 class TeacherAssistantBot : TelegramLongPollingBot() {
 
+    @Value("\${telegram.bot.name}")
+    private val telegramBotName: String = ""
+
+    @Value("\${telegram.bot.token}")
+    private val telegramBotToken: String = ""
+
     override fun getBotUsername(): String {
-        return "Teacher Assistant Bot"
+        return telegramBotName
     }
 
     override fun getBotToken(): String {
-        return ""
+        return telegramBotToken
     }
 
     override fun onUpdateReceived(p0: Update?) {
         logger.info("Update received")
+        logger.error("Oops")
     }
 
     companion object {
-        private val logger = LogManager.getLogger(TeacherAssistantBot::class.java)
+        private val logger = LoggerFactory.getLogger(TeacherAssistantBot::class.java)
     }
 }
